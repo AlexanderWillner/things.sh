a05aaa5c2349a90362cf43d9d4e815f2b540096f
Alexander Willner <alex@willner.ws>
2017-12-28 02:19:12 +0100

    debugging travis

--- test/thingsTest.sh
+++ test/thingsTest.sh
@@ -24,57 +24,57 @@ testStat() {
   command="stat"
   output=$(eval "$CLI" "$command")
   
-  expected="Inbox		:       1"
+  expected="Inbox		:       0"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected' in '\n$output'" $result
   expected="Today		:       1"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Upcoming	:       3"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Next		:       6"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Someday		:       4"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Completed	:       1"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Cancelled	:       1"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Trashed		:       3"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Tasks		:      11"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Subtasks	:       2"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Waiting		:       1"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Projects	:       4"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Repeating	:       2"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Nextish		:       6"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Headings	:       2"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Oldest     	: 2017-12-27|Today Project|Today Todo"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
   expected="Farest     	: 2045-05-13|(No Context)|Next Todo"
   (echo "$output"|grep "$expected" > /dev/null 2>&1); result=$?
-  assertEquals "Command '$command' should contain '$expected'" 0 $result
+  assertTrue "Command '$command' should contain '$expected'" $result
 }
 
 testCSV() {
