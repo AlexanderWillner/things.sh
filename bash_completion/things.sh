374816061af88b22fd9c514c743966db58a58f27
Alexander Willner <alex@willner.ws>
2018-01-01 15:55:59 +0100

    initial version of a bash completion script

---
+++ bash_completion/things.sh
@@ -0,0 +1,28 @@
+_things_complete()
+{
+    local cur_word prev_word type_list
+
+    # COMP_WORDS is an array of words in the current command line.
+    # COMP_CWORD is the index of the current word (the one the cursor is
+    # in). So COMP_WORDS[COMP_CWORD] is the current word; we also record
+    # the previous word here, although this specific script doesn't
+    # use it yet.
+    cur_word="${COMP_WORDS[COMP_CWORD]}"
+    prev_word="${COMP_WORDS[COMP_CWORD-1]}"
+
+    # Ask pss to generate a list of types it supports
+    type_list=`things.sh show-parameters`
+    command_list=`things.sh show-commands`
+
+    # Only perform completion if the current word starts with a dash ('-'),
+    # meaning that the user is trying to complete an option.
+    if [[ ${cur_word} == -* ]] ; then
+        # COMPREPLY is the array of possible completions, generated with
+        # the compgen builtin.
+        COMPREPLY=( $(compgen -W "${type_list}" -- ${cur_word}) )
+    else
+        COMPREPLY=( $(compgen -W "${command_list}" -- ${cur_word}) )
+    fi
+    return 0
+}
+complete -F _things_complete things.sh
